5acbcac6110077f9c2e8849fc181875d6ec87725
sharomet <sharometal@gmail.com>
2019-03-11 16:16:28 +0200

    Created project

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase'
+
+const app = initializeApp({
+    apiKey: "AIzaSyCD9xd7TDnZYXDf3ifbfIfns8hHrKUnccc",
+    authDomain: "vuefirebase-30953.firebaseapp.com",
+    databaseURL: "https://vuefirebase-30953.firebaseio.com",
+    projectId: "vuefirebase-30953",
+    storageBucket: "vuefirebase-30953.appspot.com",
+    messagingSenderId: "131192334061"
+});
+
+export const db = app.database()
+export const usersRef = db.ref('users')
---
+++ src/views/Home.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6">
+        <div class="card">
+          <form class="card-body" @submit.prevent="submitUser">
+            <div class="form-row">
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="firstName">First Name:</label>
+                  <input type="text" class="form-control" id="firstName" v-model="firstName">
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <label for="lastName">Last Name:</label>
+                  <input type="text" class="form-control" id="lastName" v-model="lastName">
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" class="form-control" id="email" v-model="email">
+            </div>
+            <div class="form-group mb-0 text-right">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <ul>
+          <li v-for="(user, index) in users" :key="index">
+            {{ user.firstName }} | {{ user.lastName }} | {{ user.email }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { usersRef } from '@/firebase'
+
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+    }
+  },
+  firebase: {
+    users: usersRef
+  },
+  methods: {
+    submitUser() {
+      usersRef.push({
+        firstName:  this.firstName,
+        lastName:   this.lastName,
+        email:      this.email
+      });
+      this.firstName = ''
+      this.lastName = ''
+      this.email = ''
+    }
+  }
+};
+</script>
